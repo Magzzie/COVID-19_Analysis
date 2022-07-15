839cda3ac31db62df79a867f2bf014ce1d4b8deb
Haley Miller <haleyxmiller@gmail.com>
2022-07-14 20:31:50 -0400

    connecting database to python

--- queries.sql
+++ queries.sql
@@ -1,4 +1,33 @@
---Sample query to delete rows with invalid country names (referenced from original cleaning ipynb file).
---Query Unsuccessful
-DELETE FROM total_data 
-WHERE country = "Africa";
+-- Selecting all values that are not countries
+SELECT * FROM total_data
+WHERE location in ('Africa', 'Asia', 'Europe', 
+				  'North America', 'Oceania', 'South America',
+				  'High income', 'Low income', 
+				  'Lower middle income', 'Upper middle income',
+				  'European Union', 'International', 'World');
+
+--Selecting all values that are coutnries
+SELECT * FROM total_data
+WHERE location not in ('Africa', 'Asia', 'Europe', 
+				  'North America', 'Oceania', 'South America',
+				  'High income', 'Low income', 
+				  'Lower middle income', 'Upper middle income',
+				  'European Union', 'International', 'World');
+
+--Putting not countries into a separate table
+INSERT INTO not_countries_data
+SELECT * FROM total_data
+WHERE location in ('Africa', 'Asia', 'Europe', 
+				  'North America', 'Oceania', 'South America',
+				  'High income', 'Low income', 
+				  'Lower middle income', 'Upper middle income',
+				  'European Union', 'International', 'World');
+
+--Putting only countries into a separate table
+INSERT INTO countries_data
+SELECT * FROM total_data
+WHERE location NOT IN ('Africa', 'Asia', 'Europe', 
+				  'North America', 'Oceania', 'South America',
+				  'High income', 'Low income', 
+				  'Lower middle income', 'Upper middle income',
+				  'European Union', 'International', 'World');
